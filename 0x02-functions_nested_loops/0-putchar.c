776f4ddf977f6e5f255caf2e28f7965152c316f8
S Sunday M <sarahsmoses4@gmail.com>
2023-02-21 09:59:19 +0200

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * putchar - calls standard _putchar function. outputs a character
+ * @c: character value appendable to string
+*/
+
+int _putchar(char c){
+    return putchar(c);
+}
+
+/**
+ * main - Entry point of program
+ * 
+ * Return 0 on success
+*/
+
+int main(){
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return 0;
+
+}
